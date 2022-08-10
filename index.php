51c45deab234381cbf3c1b86f92f4f6f01154960
Gabriel Ramos dos Santos <grsantos2215@gmail.com>
2022-08-10 08:58:37 -0300

    update sidemenu

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500;700&family=Rubik:wght@400;700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 
-<body id="body-pd">
+<body>
 	<!-- IMPORT SIDE MENU -->
 	<?php require_once("components/sidemenu.php"); ?>
 	<!-- END IMPORT SIDE MENU -->
