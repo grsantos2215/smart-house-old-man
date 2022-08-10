63c36403512d4ef0f9e6a8ece18a1837b7d5fb27
Gabriel Ramos dos Santos <grsantos2215@gmail.com>
2022-08-09 22:04:23 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body id="body-pd">
+	<!-- IMPORT SIDE MENU -->
+	<?php require_once("components/sidemenu.php"); ?>
+	<!-- END IMPORT SIDE MENU -->
+
+	<!-- IMPORT MAIN CONTENT -->
+	<?php require_once("components/maincontent.php"); ?>
+	<!-- END IMPORT MAIN CONTENT -->
+
+	<!-- IMPORT JS LIBRARIES -->
+	<?php require_once("components/imports.php"); ?>
+	<!-- END IMPORT JS LIBRARIES -->
+</body>
+
+</html>
