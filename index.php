2d63e26094120b299a99a752bdcc9bb340bad29f
Gabriel Ramos dos Santos <grsantos2215@gmail.com>
2022-08-18 08:01:53 -0300

    add the elements on index

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 	<link rel="stylesheet" href="assets/css/style.css">
 	<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/1413796360.js" crossorigin="anonymous"></script>
 
 </head>
 
